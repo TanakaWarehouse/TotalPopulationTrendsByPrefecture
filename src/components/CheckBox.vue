0797e11aa3891e213ba2764fd5fdc18e3cfa977f
TanakaWarehouse <116143772+TanakaWarehouse@users.noreply.github.com>
2024-03-27 16:44:43 +0900

    UpdateCheckBox.vue
    サイト内にAPIから取得した都道府県のデータを表示させることに成功

--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -3,13 +3,23 @@
 import axios from 'axios'
 
 export default{
+    //data () メソッド内でオブジェクトを定義．returnすることでデータの保持をして、vueのcomponentで必要なときにデータを引っ張ってきて使うことができる。
+    data(){
+        return{
+            prefectures: []
+        }
+    },
     mounted(){
         //JavaScriptではconstでも中身変化することあり→大文字使わないほうがよさそう．
         const prefectures_url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
         axios.get(prefectures_url,{headers: {'X-API-KEY': '8VMwXkVSFZEWRwjiYIHdOa5FlYkgrXTq1YXkBnO6'}})
-        .then(response => console.log(response));
+        
+        //response.dataに実際のデータが入っている
+        .then(response => this.prefectures = response.data.result);
     }
 }
 </script>
 
-<template></template>
+<template>
+    <h2>{{ this.prefectures }}</h2>
+</template>
