054d8398251e6a70f860f6f9df220b0809103f32
TanakaWarehouse <116143772+TanakaWarehouse@users.noreply.github.com>
2024-03-27 16:07:51 +0900

    GetNameOfPrefectures
    RESAS APIから都道府県名を取得し，コンソールに表示させることに成功

--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -1,1 +1,15 @@
+<script>
+//axiosの使用はAPIからデータを取得して表示することに対しての一般的アプローチ
+import axios from 'axios'
+
+export default{
+    mounted(){
+        //JavaScriptではconstでも中身変化することあり→大文字使わないほうがよさそう．
+        const prefectures_url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
+        axios.get(prefectures_url,{headers: {'X-API-KEY': '8VMwXkVSFZEWRwjiYIHdOa5FlYkgrXTq1YXkBnO6'}})
+        .then(response => console.log(response));
+    }
+}
+</script>
+
 <template></template>
